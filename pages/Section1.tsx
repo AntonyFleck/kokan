226a7786447df83b3ec2715741faf10c38298f8a
AntonyFleck <anirudhardesai@gmail.com>
2024-11-08 02:52:40 +0530

    fixed raw URL issue

--- pages/Section1.tsx
+++ pages/Section1.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Section1() {
   return (
-    <div className="relative h-full bg-cover bg-center  sm:bg-cover md:bg-cover bg-[url('https://github.com/AntonyFleck/kokan/blob/main/public/images/photo.jpg')] flex flex-col">
+    <div className="relative h-full bg-cover bg-center  sm:bg-cover md:bg-cover bg-[url('https://raw.githubusercontent.com/AntonyFleck/kokan/main/public/images/photo.jpg')] flex flex-col">
       {/* <div className="absolute inset-0"></div> */}
       <div className="flex flex-col flex-1 items-center justify-center pb-20 relative">
         <Title />
