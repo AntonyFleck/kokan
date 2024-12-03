d61d18ca5fa35a0c4607216b320783c2d4a04ebb
AntonyFleck <anirudhardesai@gmail.com>
2024-12-03 17:54:44 +0530

    logo add attempt 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="public/logo.png" />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="keywords"
           content={[
