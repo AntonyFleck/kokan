5453ec1a36b0ca3e6ab6a33f1c30eb2bdb09542a
AntonyFleck <anirudhardesai@gmail.com>
2024-12-03 19:24:31 +0530

    changed the main bg-imgc back to normal from git

--- pages/Section1.tsx
+++ pages/Section1.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Section1() {
   return (
-    <div className="relative h-full bg-cover bg-center  sm:bg-cover md:bg-cover bg-[url('https://raw.githubusercontent.com/AntonyFleck/kokan/main/public/images/photo.jpg')] flex flex-col">
+    <div className="relative h-full bg-cover bg-center  sm:bg-cover md:bg-cover bg-[url('/images/photo.jpg')] flex flex-col">
       {/* <div className="absolute inset-0"></div> */}
       <div className="flex flex-col flex-1 items-center justify-center pb-20 relative">
         <Title />
@@ -12,3 +12,4 @@ export default function Section1() {
     </div>
   );
 }
+// bg-[url('https://raw.githubusercontent.com/AntonyFleck/kokan/main/public/images/photo.jpg')]
