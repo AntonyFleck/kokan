983ceeefc8f1fadb6b411e8cc1f5f56a99babf56
AntonyFleck <anirudhardesai@gmail.com>
2025-01-18 19:21:51 +0530

    added google ads wala tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -114,30 +114,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag */}
-        <script
-          id="google-tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5Q4VV378');
-      `,
-          }}
-        />
-      </head>
+      <head>{/* Google Tag */}</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5Q4VV378"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}></iframe>
-        </noscript>
+        {/* Google Tag Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-G99HFETPKF"
+        />
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G99HFETPKF');
+            `,
+          }}
+        />
 
         {children}
         <Analytics />
