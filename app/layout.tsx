12c38d6992ee512daf6f1c30af5b8af8d3047f64
AntonyFleck <anirudhardesai@gmail.com>
2025-03-14 16:27:31 +0530

    added gtm tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,6 +116,24 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WGTCN3MK');
+          `,
+          }}
+        />
+        {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-83E0HH269X"></script>
         <script
@@ -125,7 +143,7 @@ export default function RootLayout({
                     gtag('js', new Date());
                     gtag('config', 'G-83E0HH269X');`,
           }}
-        />
+        /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
@@ -145,7 +163,14 @@ export default function RootLayout({
             `,
           }}
         /> */}
-
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WGTCN3MK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Analytics />
       </body>
